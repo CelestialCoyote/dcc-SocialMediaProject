ff4e113d62343264d1fe68cc757e51561b3a5831
Byakuya91 <nganti91@yahoo.com>
2022-05-09 14:11:18 -0400

    I've successfully created a Post route and can post data via Postman to the Post collections.

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const connectDb = require("./db/db");
 const usersRouter = require("./routes/users");
+const postsRouter = require("./routes/posts");
 const express = require("express");
 const cors = require("cors");
 const app = express();
@@ -10,6 +11,7 @@ connectDb();
 app.use(cors());
 app.use(express.json());
 app.use(`/api/users`, usersRouter);
+app.use(`/api/posts`, postsRouter);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
