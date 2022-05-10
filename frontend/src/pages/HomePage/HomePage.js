dbd02db63e3733e4821bea7029d7b6aeb7da8287
Paul Stearns <coyote@Pauls-MBP.routerlogin.net>
2022-05-10 18:38:00 -0400

    PostForm

--- frontend/src/pages/HomePage/HomePage.js
+++ frontend/src/pages/HomePage/HomePage.js
@@ -1,10 +1,21 @@
 import React from "react";
 import { useContext } from "react";
 import AuthContext from "../../context/AuthContext";
+import PostForm from '../../components/PostForm/PostForm';
+
 
 const HomePage = () => {
   const { user } = useContext(AuthContext);
-  return <h1 className="container">Home Page for {user.name}!</h1>;
+
+  return (
+
+    <div>
+        <h1 className="container">Home Page for {user.name}!</h1>
+        <PostForm />
+    </div>
+
+  );
+
 };
 
 export default HomePage;
