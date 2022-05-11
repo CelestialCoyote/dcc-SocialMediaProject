474f23e428da92317650a09a734c42a0f1c9dc68
Paul Stearns <coyote@Pauls-MBP.routerlogin.net>
2022-05-10 23:27:05 -0400

    Started working on login page funtionality for user in database.

--- frontend/src/pages/LoginPage/LoginPage.js
+++ frontend/src/pages/LoginPage/LoginPage.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import React, { useContext, useEffect } from "react";
 import AuthContext from "../../context/AuthContext";
 import useCustomForm from "../../hooks/useCustomForm";
@@ -5,48 +6,60 @@ import { Link } from "react-router-dom";
 import "./LoginPage.css";
 
 const LoginPage = () => {
-  const { loginUser, isServerError } = useContext(AuthContext);
-  const defaultValues = { email: "", password: "" };
-  const [formData, handleInputChange, handleSubmit, reset] = useCustomForm(
-    defaultValues,
-    loginUser
-  );
+    const { loginUser, isServerError } = useContext(AuthContext);
+    const defaultValues = { email: "", password: "" };
+    const [formData, handleInputChange, handleSubmit, reset] = useCustomForm(
+        defaultValues,
+        loginUser,
+        handleUserLogin
+    );
 
-  useEffect(() => {
-    if (isServerError) {
-      reset();
+    useEffect(() => {
+        if (isServerError) {
+            reset();
+        }
+    }, [isServerError]);
+
+    async function handleUserLogin() {
+        try {
+            const jwt = localStorage.getItem('token');
+            const response = await axios
+                .post("http://localhost:3011/api/users/login", { headers: { Authorization: 'Bearer ' + jwt } });
+            console.log(response.data);
+        } catch (error) {
+            console.log(error.message);
+        }
     }
-  }, [isServerError]);
 
-  return (
-    <div className="container">
-      <form className="form" onSubmit={handleSubmit}>
-        <label>
-          Email:{" "}
-          <input
-            type="text"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          Password:{" "}
-          <input
-            type="text"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-        </label>
-        {isServerError ? (
-          <p className="error">Login failed, incorrect credentials!</p>
-        ) : null}
-        <Link to="/register">Click to register!</Link>
-        <button>Login!</button>
-      </form>
-    </div>
-  );
+    return (
+        <div className="container">
+            <form className="form" onSubmit={handleSubmit}>
+                <label>
+                    Email:{" "}
+                    <input
+                        type="text"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                <label>
+                    Password:{" "}
+                    <input
+                        type="text"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                {isServerError ? (
+                    <p className="error">Login failed, incorrect credentials!</p>
+                ) : null}
+                <Link to="/register">Click to register!</Link>
+                <button>Login!</button>
+            </form>
+        </div>
+    );
 };
 
 export default LoginPage;
--- frontend/src/pages/RegisterPage/RegisterPage.js
+++ frontend/src/pages/RegisterPage/RegisterPage.js
@@ -4,76 +4,76 @@ import AuthContext from "../../context/AuthContext";
 import useCustomForm from "../../hooks/useCustomForm";
 
 const RegisterPage = () => {
-  const { registerUser } = useContext(AuthContext);
-  const defaultValues = { name: "", email: "", password: "", isAdmin: false };
-  const [formData, handleInputChange, handleSubmit] = useCustomForm(
-    defaultValues,
-    registerUser,
-    handleUserInfo
-  );
+    const { registerUser } = useContext(AuthContext);
+    const defaultValues = { name: "", email: "", password: "", isAdmin: false };
+    const [formData, handleInputChange, handleSubmit] = useCustomForm(
+        defaultValues,
+        registerUser,
+        handleUserInfo
+    );
 
-  async function handleUserInfo() {
-    try {
-      let newUser = {};
-      await axios
-        .post("http://localhost:3011/api/users/register")
-        .then((res) => {
-          newUser = res.data;
-        });
-    } catch (error) {
-      console.log(error.message);
+    async function handleUserInfo() {
+        try {
+            let newUser = {};
+            await axios
+                .post("http://localhost:3011/api/users/register")
+                .then((res) => {
+                    newUser = res.data;
+                });
+        } catch (error) {
+            console.log(error.message);
+        }
     }
-  }
 
-  return (
-    <div className="container">
-      <form className="form" onSubmit={handleSubmit}>
-        <label>
-          Name:{" "}
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          Email:{" "}
-          <input
-            type="text"
-            name="email"
-            value={formData.email}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label>
-          Password:{" "}
-          <input
-            type="text"
-            name="password"
-            value={formData.password}
-            onChange={handleInputChange}
-          />
-        </label>
-        <label
-          style={{
-            display: "flex",
-            alignItems: "center",
-            width: "20%",
-          }}
-        >
-          Admin:{" "}
-          <input
-            type="checkbox"
-            name="isAdmin"
-            checked={formData.isAdmin}
-            onChange={handleInputChange}
-          />
-        </label>
-        <button type="submit">Register!</button>
-      </form>
-    </div>
-  );
+    return (
+        <div className="container">
+            <form className="form" onSubmit={handleSubmit}>
+                <label>
+                    Name:{" "}
+                    <input
+                        type="text"
+                        name="name"
+                        value={formData.name}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                <label>
+                    Email:{" "}
+                    <input
+                        type="text"
+                        name="email"
+                        value={formData.email}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                <label>
+                    Password:{" "}
+                    <input
+                        type="text"
+                        name="password"
+                        value={formData.password}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                <label
+                    style={{
+                        display: "flex",
+                        alignItems: "center",
+                        width: "20%",
+                    }}
+                >
+                    Admin:{" "}
+                    <input
+                        type="checkbox"
+                        name="isAdmin"
+                        checked={formData.isAdmin}
+                        onChange={handleInputChange}
+                    />
+                </label>
+                <button type="submit">Register!</button>
+            </form>
+        </div>
+    );
 };
 
 export default RegisterPage;
